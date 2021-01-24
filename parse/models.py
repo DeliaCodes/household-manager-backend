91283d4418bab5b9558dc32158a43ed42dad1edf
Delia Codes <delia@delia.codes>
2021-01-24 18:53:44 -0500

    Adds Email Receipt Model based on Sendgrid's inbound parse docs.

--- parse/models.py
+++ parse/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+
+class EmailReceiptSendgrid(models.Model):
+    headers = models.TextField()
+    dkim = models.CharField(max_length=none)
+    content_ids = models.CharField(max_length=none)
+    to = models.CharField(max_length=none)
+    text = models.TextField()
+    # @todo is JSONField right?
+    html = models.JSONField()
+    from = models.CharField(max_length=none)
+    sender_ip = models.CharField(max_length=none)
+    spam_report = models.TextField()
+    envelope = JSONField()
+    attachments = models.IntegerField()
+    subject = models.CharField(max_length=none)
+    spam_score = models.CharField(max_length=none)
+    attachment_info = models.JSONField()
+    charsets = models.CharField(max_length=none)
+    spf = models.CharField(max_length=none)
+
+    def __str__(self):
+        return self.subject
+
+
+class User(models.Model):
+    name = models.CharField(max_length=2000)
+    email_address = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
